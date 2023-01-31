ddd8b6951e24eb1a2ef277183229ba575257864f
Hamza Mehmood Anwar <hamzamehmood.anwar@systemsltd.com>
2023-01-31 12:36:05 +0500

    client side data fetching using SWR

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,40 +1,35 @@
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSales = () => {
-  const [salesData, setSalesData] = useState();
+  const [salesData, setSalesData] = useState([]);
 
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, error, isLoading } = useSWR(
+    "https://nextjs-demo-9a278-default-rtdb.firebaseio.com/sales.json",
+    (apiURL: string) => fetch(apiURL).then((res) => res.json())
+  );
 
   useEffect(() => {
-    const abortController = new AbortController();
-    const signal = abortController.signal;
+    if (data) {
+      const transformedData = [];
+      for (const key in data) {
+        transformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-    setIsLoading(true);
-    fetch("https://nextjs-demo-9a278-default-rtdb.firebaseio.com/sales.json", {
-      signal,
-    })
-      .then((data: any) => data.json())
-      .then((result) => {
-        console.log(result);
-        const transformedData = [];
-        for (const key in result) {
-          transformedData.push({
-            id: key,
-            username: result[key].username,
-            volume: result[key].volume,
-          });
-        }
-        setIsLoading(false);
-        setSalesData(transformedData);
-      });
-    return () => {
-      abortController.abort();
-    };
-  }, []);
+      setSalesData(transformedData);
+    }
+  }, [data]);
+
+  if (error) return "ERROR!!!";
+  if (!data || !salesData) return " loading.........";
   if (isLoading) return "loading....";
   return (
     <div>
-      {salesData.map((sale: any) => {
+      {salesData?.map((sale: any) => {
         return (
           <p>
             {sale.username} <span>{sale.volume}</span>
