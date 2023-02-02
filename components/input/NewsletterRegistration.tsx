e076ba831c3a5be19f7daeaacdf6caa6246d3768
Hamza Mehmood Anwar <hamzamehmood.anwar@systemsltd.com>
2023-02-02 16:16:52 +0500

    newsletter api added

--- components/input/NewsletterRegistration.tsx
+++ components/input/NewsletterRegistration.tsx
@@ -1,12 +1,21 @@
+import { useRef } from "react";
 import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
+  const emailRef: any = useRef();
   function registrationHandler(event: any) {
     event.preventDefault();
-
     // fetch user input (state or refs)
     // optional: validate input
     // send valid data to API
+    const email = emailRef.current.value;
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   return (
@@ -15,6 +24,7 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
+            ref={emailRef}
             type="email"
             id="email"
             placeholder="Your email"
